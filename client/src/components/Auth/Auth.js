59d092e00fb465c1c987c02f0149164ee3e24883
Balu Deshamoni <88838021+BaluDeshamoni@users.noreply.github.com>
2022-05-26 22:22:16 +0530

    Update Auth.js

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -138,25 +138,6 @@ const SignUp = () => {
           >
             {isSignup ? 'Sign Up' : 'Sign In'}
           </Button>
-          /*<GoogleLogin
-            clientId='564033717568-bu2nr1l9h31bhk9bff4pqbenvvoju3oq.apps.googleusercontent.com'
-            render={(renderProps) => (
-              <Button
-                className={classes.googleButton}
-                color='primary'
-                fullWidth
-                onClick={renderProps.onClick}
-                disabled={renderProps.disabled}
-                startIcon={<Icon />}
-                variant='contained'
-              >
-                Google Sign In
-              </Button>
-            )}
-            onSuccess={googleSuccess}
-            onFailure={googleError}
-            cookiePolicy='single_host_origin'
-          />*/
           <Grid container justifyContent='flex-end'>
             <Grid item>
               <Button onClick={switchMode}>
